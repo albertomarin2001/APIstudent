a35fd22fb9ce193c2de77c16b10834b07118e236
Alberto Marin Estremadoiro <a2019116197@estudiantes.upsa.edu.bo>
2020-08-22 13:48:37 -0400

    WEB API

---
+++ APIstudent/APIstudent/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIstudent.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+
+
+        [StringLength(30)]
+        [Required(ErrorMessage = "You Must Enter The Field {0}")]
+        public string LastName { get; set; }
+
+
+        [StringLength(30, ErrorMessage = "The Field {0} Must Contain Between {2} and {1} Characters", MinimumLength = 2)]// EL 0 ES LA REFERENCIA
+        [Required(ErrorMessage = "You Must Enter The Field {0}")]
+        public string FirstName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}",ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+    }
+}
